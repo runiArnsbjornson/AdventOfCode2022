27767bfa98ba36dca375d723bf535eb2df216495
Julien Debladis <julien.debladis@gmail.com>
2023-04-04 11:04:00 +0200

    improvement of day3
    following the guidelines of my master, here are the corrections requested

--- Runi/day3.dart
+++ Runi/day3.dart
@@ -1,39 +1,45 @@
 import 'dart:io';
 
 void main() {
-  List<String> listInput = File('../input/day3_input.txt').readAsLinesSync();
+  final List<String> listInput =
+      File('../input/day3_input.txt').readAsLinesSync();
 
+  print('The sum of priorities is : ${solutionPart1(listInput)}');
+  print('The sum of safety badges is : ${solutionPart2(listInput)}');
+}
+
+int solutionPart1(List<String> listInput) {
   String solutionPart1 = '';
   for (final input in listInput) {
     int halfLength = input.length ~/ 2;
-    solutionPart1 += find(input.substring(0, halfLength),
-        input.substring(halfLength, input.length));
+    solutionPart1 += findCommonRunes(
+        input.substring(0, halfLength), input.substring(halfLength));
   }
-  print('The sum of priorities is : ${sum(solutionPart1)}');
-
-  String solutionPart2 = '';
-  for (var i = 2; i < listInput.length; i++) {
-    if ((i + 1) % 3 == 0) {
-      solutionPart2 +=
-          find(listInput[i], find(listInput[i - 1], listInput[i - 2]));
-    }
-  }
-  print('The sum of safety badges is : ${sum(solutionPart2)}');
+  return sumPriority(solutionPart1);
 }
 
-/// find the common runes in 2 Strings
-String find(String compartment1, String compartment2) {
+int solutionPart2(List<String> listInput) {
+  String solutionPart2 = '';
+  for (int i = 2; i < listInput.length; i++) {
+    if ((i + 1) % 3 == 0) {
+      solutionPart2 += findCommonRunes(
+          listInput[i], findCommonRunes(listInput[i - 1], listInput[i - 2]));
+    }
+  }
+  return sumPriority(solutionPart2);
+}
+
+String findCommonRunes(String str1, String str2) {
   String commons = '';
-  for (int i = 0; i < compartment1.length; i++) {
-    if (compartment2.contains(compartment1[i]) &&
-        !commons.contains(compartment1[i])) {
-      commons += compartment1[i];
+  for (int i = 0; i < str1.length; i++) {
+    if (str2.contains(str1[i]) && !commons.contains(str1[i])) {
+      commons += str1[i];
     }
   }
   return commons;
 }
 
-int sum(String str) {
+int sumPriority(String str) {
   String ref = '0abcdefghijklmnopqrstuvwxyz';
   ref += ref.substring(1).toUpperCase();
   int sum = 0;
